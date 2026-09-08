a47191b340ad8113bd4d14b004d91a625554d5f8
NicolySaar <saarnicoly@gmail.com>
2026-09-08 19:03:05 -0300

    Add double, triple, and square root calculations

---
+++ guanabara/double.py
@@ -0,0 +1,5 @@
+num = float(input('Digite um número: '))
+
+print('O dobro de {} é {}'.format(num, (num*2)))
+print('O triplo de {} é {}'.format(num, (num*3)))
+print('A raiz quadrada de {} é {:.2f}'.format(num, (num**(1/2))))
