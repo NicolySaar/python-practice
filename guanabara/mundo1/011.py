57fd83c2bef92d46e6e714e707c1c16c8705d636
Nicoly Saar <saarnicoly@gmail.com>
2026-09-20 13:46:47 -0300

    Delete guanabara/mundo1 directory

--- guanabara/mundo1/011.py
+++
@@ -1,9 +0,0 @@
-largura = float(input('Insira a largura da parede: '))
-altura = float(input('Insira a altura da parede: '))
-área = largura * altura
-tinta = área / 2
-
-
-
-print('A parede tem dimensão de {:.1f}x{:.1f} e sua área é de {:.1f}m².'.format(largura, altura, área))
-print('Apartir disso, é possível concluir que é preciso de {:.1f}L de tinta para pintar a parede.'.format(tinta))
