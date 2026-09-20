b27cfc4396a41e5306d7ddc491ffa1ad487ce28d
Nicoly Saar <saarnicoly@gmail.com>
2026-09-20 13:39:04 -0300

    Delete guanabara directory

--- guanabara/soma.py
+++
@@ -1,4 +0,0 @@
-num1 = int(input("Digite um número: "))
-num2 = int(input("Digite outro número: "))
-
-print("A soma de {} e {} é {}".format(num1, num2, (num1+num2)))
--- guanabara/text.py
+++
@@ -1,9 +0,0 @@
-nome = str(input("Digite o seu nome: ")).strip()
-
-print("Analisando seu nome...")
-print("Seu nome em letras maiúsculas é {}".format(nome.upper()))
-print("Seu nome em letra minúsculas é {}".format(nome.lower()))
-print("Seu nome tem {} letras".format(len(nome) - nome.count(' ')))
-
-nomesep = nome.split()
-print("Seu primeiro nome é {} e tem {} letras".format(nomesep[0], len(nomesep[0])))
